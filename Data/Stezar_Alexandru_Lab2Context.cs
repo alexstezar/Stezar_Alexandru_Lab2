7a2deeed65ae9499557d6da8c803190714d308d8
Alex Stezar <alex.stezar07@gmail.com>
2023-10-26 18:33:22 +0300

    Add project files.

---
+++ Data/Stezar_Alexandru_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stezar_Alexandru_Lab2.Models;
+
+namespace Stezar_Alexandru_Lab2.Data
+{
+    public class Stezar_Alexandru_Lab2Context : DbContext
+    {
+        public Stezar_Alexandru_Lab2Context (DbContextOptions<Stezar_Alexandru_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stezar_Alexandru_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Stezar_Alexandru_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stezar_Alexandru_Lab2.Data;
+using Stezar_Alexandru_Lab2.Models;
+
+namespace Stezar_Alexandru_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stezar_Alexandru_Lab2.Data.Stezar_Alexandru_Lab2Context _context;
+
+        public IndexModel(Stezar_Alexandru_Lab2.Data.Stezar_Alexandru_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
